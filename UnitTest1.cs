2391701ffb73de9c221dbf80d970eaec96bd07a6
gstyanti <101311934+gstyanti@users.noreply.github.com>
2022-10-20 22:30:31 +0700

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace QuickTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetCurrentDate()
+        {
+            Assert.AreEqual(DateTime.Now.Date, QuickDate.Calender.GetCurrentDate());
+        }
+    }
+}
